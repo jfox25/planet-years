d8bda4932c189f20f0aaacdb64d4183d76c017bd
James Fox <jimmyfox00@gmail.com>
2022-04-15 09:08:16 -0700

    test written for returning age on Venus

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -12,4 +12,11 @@ describe("Person", () => {
     const mercuryAge = person.ageOnPlanet(0.24);
     expect(mercuryAge).toEqual(87.5);
   });
+
+  test("should return the age of a person object on Venus", () => {
+    const person = new Person(21);
+
+    const mercuryAge = person.ageOnPlanet(0.62);
+    expect(mercuryAge).toEqual(87.5);
+  });
 });
