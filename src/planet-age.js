beb66391f333017be20fca3972d3ca108138dfc1
James Fox <jimmyfox00@gmail.com>
2022-04-15 16:50:07 -0700

    Removed unused code

--- src/planet-age.js
+++ src/planet-age.js
@@ -5,16 +5,12 @@ export class Person {
     this.planetArray = [
       "Mercury",
       0.24,
-      "../assets/images/mercueryRotation.gif",
       "Venus",
       0.62,
-      "../assets/images/venusRotation.gif",
       "Mars",
       1.88,
-      "../assets/images/marsRotation.gif",
       "Jupiter",
       11.86,
-      "../assets/images/jupiter.gif",
     ];
     this.ageArray = [];
     if (lifeExpectancy < age) {
@@ -30,14 +26,12 @@ export class Person {
   }
 
   calculateAllAges() {
-    for (let i = 0; i < this.planetArray.length; i += 3) {
+    for (let i = 0; i < this.planetArray.length; i += 2) {
       let array = [];
       const planetAge = this.ageOnPlanet(this.planetArray[i + 1], this.age);
       array.push(planetAge);
       array.push(this.leftToLive(planetAge, this.planetArray[i + 1]));
       array.push(this.planetArray[i]);
-      array.push(this.planetArray[i + 2]);
-      array.push;
       this.ageArray.push(array);
     }
   }
