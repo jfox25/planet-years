55d88756cc2e2097d4e35433b1540f7c3692ec83
James Fox <jimmyfox00@gmail.com>
2022-04-15 09:05:41 -0700

    test written for getting age on mercury

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -5,4 +5,11 @@ describe("Person", () => {
     const person = new Person(21);
     expect(person.age).toEqual(21);
   });
+
+  test("should return the age of a person object on Mercury", () => {
+    const person = new Person(21);
+
+    const mercuryAge = person.ageOnPlanet(0.24);
+    expect(mercuryAge).toEqual(87.5);
+  });
 });
