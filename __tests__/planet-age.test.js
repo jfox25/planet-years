75532eb5b7912b5878b4845f546b3d2ac4384375
James Fox <jimmyfox00@gmail.com>
2022-04-15 10:08:00 -0700

    test for life expency remaining on mercury written

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -8,35 +8,35 @@ describe("Person", () => {
   });
 
   test("should return the age of a person object on Mercury", () => {
-    const person = new Person(21);
+    const person = new Person(21, 75);
 
     const mercuryAge = person.ageOnPlanet(0.24);
     expect(mercuryAge).toEqual(87.5);
   });
 
   test("should return the age of a person object on Venus", () => {
-    const person = new Person(21);
+    const person = new Person(21, 75);
 
     const venusAge = person.ageOnPlanet(0.62);
     expect(venusAge).toEqual(33.9);
   });
 
   test("should return the age of a person object on Mars", () => {
-    const person = new Person(21);
+    const person = new Person(21, 75);
 
     const marsAge = person.ageOnPlanet(1.88);
     expect(marsAge).toEqual(11.2);
   });
 
   test("should return the age of a person object on Jupiter", () => {
-    const person = new Person(21);
+    const person = new Person(21, 75);
 
     const jupiterAge = person.ageOnPlanet(11.86);
     expect(jupiterAge).toEqual(1.8);
   });
 
   test("should set the age value for all planets on the person object", () => {
-    const person = new Person(21);
+    const person = new Person(21, 75);
     person.calculateAllAges(person.planetArray);
     console.log(person);
     expect(person.mercuryAge).toEqual(87.5);
@@ -45,12 +45,12 @@ describe("Person", () => {
     expect(person.jupiterAge).toEqual(1.8);
   });
 
-  test("should set the age value for all planets on the person object", () => {
-    const person = new Person(21);
-    person.calculateAllAges(person.planetArray);
-    expect(person.mercuryAge).toEqual(87.5);
-    expect(person.venusAge).toEqual(33.9);
-    expect(person.marsAge).toEqual(11.2);
-    expect(person.jupiterAge).toEqual(1.8);
+  test("should return how much you have left to live on mecuery", () => {
+    const person = new Person(21, 75);
+    const mercuryAge = person.ageOnPlanet(0.24);
+    const leftToLive = person.leftToLive(mercuryAge, person.lifeExpectancy);
+
+    expect(mercuryAge).toEqual(87.5);
+    expect(leftToLive).toEqual(225);
   });
 });
